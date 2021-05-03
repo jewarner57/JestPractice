a4040a5132c226bbd4196e5431ae05f0052f9258
Jonathan Warner <jewarner57@gmail.com>
2021-05-03 19:24:38 -0400

    convert js to typescript

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,67 +1,68 @@
-const FIZZ = 'fizz'
-const BUZZ = 'buzz'
-const FIZZBUZZ = 'fizzbuzz'
-
-/** 
- * Returns true when n is divisible by 3 
+var FIZZ = 'fizz';
+var BUZZ = 'buzz';
+var FIZZBUZZ = 'fizzbuzz';
+/**
+ * Returns true when n is divisible by 3
  * @param {Number} n
- * @returns {Boolean} fizziness 
+ * @returns {Boolean} fizziness
  * */
-const isFizzy = (n, fizzOn = 3) => {
-  return n % fizzOn === 0
-}
-
+var isFizzy = function (n, fizzOn) {
+    if (fizzOn === void 0) { fizzOn = 3; }
+    return n % fizzOn === 0;
+};
 /**
  * Returns true when n is divisible by 5
- * @param {Number} n 
+ * @param {Number} n
  * @returns {Boolean} buzziness
  */
-const isBuzzy = (n, buzzOn = 5) => {
-  return n % buzzOn === 0
-}
-
+var isBuzzy = function (n, buzzOn) {
+    if (buzzOn === void 0) { buzzOn = 5; }
+    return n % buzzOn === 0;
+};
 /**
  * Returns FIZZ, BUZZ, or FIZZBUZZ when a number is divisible by 3, 5, or both
  * @param {Number} n
  * @returns {String} '', 'FIZZ', 'BUZZ', or 'FIZZBUZZ'
  */
-const fizzyBuzzy = (n, fizzOn = 3, buzzOn = 5) => {
-  let result = ''
-  if (isFizzy(n, fizzOn)) { result += FIZZ }
-  if (isBuzzy(n, buzzOn)) { result += BUZZ }
-  return result
-}
-
+var fizzyBuzzy = function (n, fizzOn, buzzOn) {
+    if (fizzOn === void 0) { fizzOn = 3; }
+    if (buzzOn === void 0) { buzzOn = 5; }
+    var result = '';
+    if (isFizzy(n, fizzOn)) {
+        result += FIZZ;
+    }
+    if (isBuzzy(n, buzzOn)) {
+        result += BUZZ;
+    }
+    return result;
+};
 /**
  * Generates a results object describing a sequence of fizz buzz for count.
- * @param {Number} count 
+ * @param {Number} count
  * @returns {Object} with properties count, fizz, buzz, and fizzbuzz
  */
-const fizzBuzz = (count) => {
-  let result = { count, fizz: 0, buzz: 0, fizzBuzz: 0 }
-  for (let i = 1; i <= count; i += 1) {
-    const str = fizzyBuzzy(i)
-
-    switch (str) {
-      case FIZZ:
-        result.fizz += 1
-        break
-      case BUZZ:
-        result.buzz += 1
-        break
-      case FIZZBUZZ:
-        result.fizzBuzz += 1
-        break
+var fizzBuzz = function (count) {
+    var result = { count: count, fizz: 0, buzz: 0, fizzBuzz: 0 };
+    for (var i = 1; i <= count; i += 1) {
+        var str = fizzyBuzzy(i);
+        switch (str) {
+            case FIZZ:
+                result.fizz += 1;
+                break;
+            case BUZZ:
+                result.buzz += 1;
+                break;
+            case FIZZBUZZ:
+                result.fizzBuzz += 1;
+                break;
+        }
     }
-  }
-  return result
-}
-
-module.exports.isFizzy = isFizzy
-module.exports.isBuzzy = isBuzzy
-module.exports.fizzyBuzzy = fizzyBuzzy
-module.exports.fizzBuzz = fizzBuzz
-module.exports.FIZZ = FIZZ
-module.exports.BUZZ = BUZZ
-module.exports.FIZZBUZZ = FIZZBUZZ
-
+    return result;
+};
+module.exports.isFizzy = isFizzy;
+module.exports.isBuzzy = isBuzzy;
+module.exports.fizzyBuzzy = fizzyBuzzy;
+module.exports.fizzBuzz = fizzBuzz;
+module.exports.FIZZ = FIZZ;
+module.exports.BUZZ = BUZZ;
+module.exports.FIZZBUZZ = FIZZBUZZ;
---
+++ fizzbuzz.ts
@@ -0,0 +1,67 @@
+const FIZZ: string = 'fizz'
+const BUZZ: string = 'buzz'
+const FIZZBUZZ: string = 'fizzbuzz'
+
+/** 
+ * Returns true when n is divisible by 3 
+ * @param {Number} n
+ * @returns {Boolean} fizziness 
+ * */
+const isFizzy = (n: number, fizzOn: number = 3): boolean => {
+  return n % fizzOn === 0
+}
+
+/**
+ * Returns true when n is divisible by 5
+ * @param {Number} n 
+ * @returns {Boolean} buzziness
+ */
+const isBuzzy = (n: number, buzzOn: number = 5): boolean => {
+  return n % buzzOn === 0
+}
+
+/**
+ * Returns FIZZ, BUZZ, or FIZZBUZZ when a number is divisible by 3, 5, or both
+ * @param {Number} n
+ * @returns {String} '', 'FIZZ', 'BUZZ', or 'FIZZBUZZ'
+ */
+const fizzyBuzzy = (n: number, fizzOn: number = 3, buzzOn: number = 5): string => {
+  let result: string = ''
+  if (isFizzy(n, fizzOn)) { result += FIZZ }
+  if (isBuzzy(n, buzzOn)) { result += BUZZ }
+  return result
+}
+
+/**
+ * Generates a results object describing a sequence of fizz buzz for count.
+ * @param {Number} count 
+ * @returns {Object} with properties count, fizz, buzz, and fizzbuzz
+ */
+const fizzBuzz = (count: number): Object => {
+  let result = { count, fizz: 0, buzz: 0, fizzBuzz: 0 }
+  for (let i: number = 1; i <= count; i += 1) {
+    const str: string = fizzyBuzzy(i)
+
+    switch (str) {
+      case FIZZ:
+        result.fizz += 1
+        break
+      case BUZZ:
+        result.buzz += 1
+        break
+      case FIZZBUZZ:
+        result.fizzBuzz += 1
+        break
+    }
+  }
+  return result
+}
+
+module.exports.isFizzy = isFizzy
+module.exports.isBuzzy = isBuzzy
+module.exports.fizzyBuzzy = fizzyBuzzy
+module.exports.fizzBuzz = fizzBuzz
+module.exports.FIZZ = FIZZ
+module.exports.BUZZ = BUZZ
+module.exports.FIZZBUZZ = FIZZBUZZ
+
